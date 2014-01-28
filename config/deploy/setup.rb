8b170481af6ecf9b0c203c4e5dcfa69733ce362d
Yohei Kawahara <inokara@gmail.com>
2014-01-28 16:05:10 +0900

    modified.

--- config/deploy/setup.rb
+++ config/deploy/setup.rb
@@ -1,25 +1,20 @@
-role :servers, "xxx.xxx.xxx.1:49163","xxx.xxx.xxx.1:49164"
+role :servers, "xxx.xxxx.xxx.xxx:49153","xxx.xxx.xxx.xxx:49154"
 #
-set :user, "root"
-set :path_to, ""
+set :user, "your_user"
+set :path_to, "your_path"
 #
 set :chef_local_dir, "#{path_to}/chef-repo"
 set :chef_remote_dir, "/root/chef"
 #
 namespace :chef do
   task :init, :roles => :servers do
-    run "apt-get update && apt-get -y install sudo curl"
+    run "apt-get update && apt-get -y install sudo curl rsync"
     run "curl -L https://www.opscode.com/chef/install.sh | sudo bash"
+  end
+  task :sync, :roles => :servers do
     run "mkdir -p #{chef_remote_dir}/chef-repo/"
     find_servers_for_task(current_task).each do |server|
-      if server.include?(":")
-        arr = server.split(":")
-        sv = arr[0] 
-        port = arr[1]
-        `rsync -avz #{chef_local_dir}/ -e "ssh -p #{port}" root@#{sv}:#{chef_remote_dir}/chef-repo/`
-      else
-        `rsync -avz #{chef_local_dir}/ root@#{server}:#{chef_remote_dir}/chef-repo/`
-      end
+      `rsync -avz #{chef_local_dir}/ -e "ssh -p #{server.port}" root@#{server.host}:#{chef_remote_dir}/chef-repo/`
     end
   end
   task :deploy, :roles => :servers do
